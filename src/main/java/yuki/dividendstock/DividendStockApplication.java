6df0da02d6f7b631b43acf615539e9d0709a26dc
endlessmomo <soohyuk96@naver.com>
2022-11-10 20:02:55 +0900

    Initial commit

---
+++ src/main/java/yuki/dividendstock/DividendStockApplication.java
@@ -0,0 +1,13 @@
+package yuki.dividendstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendStockApplication.class, args);
+    }
+
+}
